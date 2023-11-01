8c6b4082e4fc2570c83841e9b45824b3b2b93db2
andereyaa <andrewmamawi@gmail.com>
2023-11-01 21:48:42 +0300

    Added logic to initialise REYAX RYLR890

--- esp32-sc-gway.ino
+++ esp32-sc-gway.ino
@@ -1,4 +1,42 @@
+#include<HardwareSerial.h>
 
+#include "esp32-sc-gway.h"      // This file contains configuration of GWay
+
+int debug = 1;									// Debug level! 0 is no msgs, 1 normal, 2 is extensive
+HardwareSerial LoRaRadio(2);
+
+// ----------------------------------------------------------------------------
+// Function to join the Wifi Network
+// ----------------------------------------------------------------------------
+int initialiseLoraRadio(){
+  LoRaRadio.begin(baudRate, SERIAL_8N1, DEFAULT_RX_PIN, DEFAULT_TX_PIN);
+  delay(delay);
+  LoRaRadio.print((String)"AT+ADDRESS=" + AT_ADDRESS + "\r\n");
+  delay(delay);
+  LoRaRadio.print((String)"AT+NETWORKID=" + AT_NETWORK_ID + "\r\n");
+  delay(delay);
+  LoRaRadio.print((String)"AT+MODE=" + AT_MODE + "\r\n");
+  delay(delay);
+  LoRaRadio.print((String)"AT+IPR=" + AT_IPR + "\r\n");
+  delay(delay);
+  LoRaRadio.print((String)"AT+CRFOP=" + AT_CRFOP + "\r\n");
+  delay(delay);
+  LoRaRadio.print((String)"AT+BAND=" + AT_BAND + "\r\n");
+  delay(delay);
+  LoRaRadio.print((String)"AT+PARAMETER=" + AT_PARAMETER + "\r\n");
+  delay(delay);
+  LoRaRadio.print("AT+CPIN=" + AT_PASSWORD + "\r\n");
+  delay(delay);
+  LoRaRadio.print("AT+CPIN?\r\n"); // confirm password is set
+
+  return 0;
+}
+// ----------------------------------------------------------------------------
+// Function to join the Wifi Network
+// ----------------------------------------------------------------------------
+int connectWifi(char* wifi_ssid, char* wifi_pass){
+
+}
 
 // ========================================================================
 // MAIN PROGRAM (SETUP AND LOOP)
@@ -6,7 +44,10 @@
 // ----------------------------------------------------------------------------
 // Setup code (one time)
 // ----------------------------------------------------------------------------
-void setup() {}
+void setup() {
+
+  Serial.print(F("\r\nBooting "));
+}
 
 
 // ----------------------------------------------------------------------------
