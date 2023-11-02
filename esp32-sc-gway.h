a0e3e10c5e2d88ae0c8ec78e9e67b92ccea3ee75
andereyaa <andrewmamawi@gmail.com>
2023-11-02 06:04:50 +0300

    Fix syntax

--- esp32-sc-gway.h
+++ esp32-sc-gway.h
@@ -32,15 +32,15 @@
 // REYAX RYLR890
 #define DEFAULT_TX_PIN
 #define DEFAULT_RX_PIN
-#define baudRate 115200;
-#define AT_ADDRESS 1;
-#define AT_NETWORK_ID 10;
-#define AT_MODE 0;
-#define AT_IPR 115200;
-#define AT_PARAMETER "12,4,1,7";
-#define AT_BAND 868500000;
-#define AT_CRFOP 10;
-#define AT_PASSWORD "92A0ECEC9000DA0DCF0CAAB0ABA2E0EF";
+#define baudRate 115200
+#define AT_ADDRESS 1
+#define AT_NETWORK_ID 10
+#define AT_MODE 0
+#define AT_IPR 115200
+#define AT_PARAMETER "12,4,1,7"
+#define AT_BAND 868500000
+#define AT_CRFOP 10
+#define AT_PASSWORD "92A0ECEC9000DA0DCF0CAAB0ABA2E0EF"
 
 // Web Server
 #define URL_TEST_SERVER ""
